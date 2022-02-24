4bddc0af266a5dfec7f738404bccb81d38beab21
silvestre1313 <patrick.silvestre13@hotmail.com>
2022-02-23 21:44:33 -0300

    First commit

---
+++ app/src/main/java/com/udemy/ifoodcl/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.udemy.ifoodcl.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udemy.ifoodcl.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
