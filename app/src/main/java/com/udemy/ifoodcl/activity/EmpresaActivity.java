5025d39bb78e85ba7c20283d788134603cefe381
silvestre1313 <patrick.silvestre13@hotmail.com>
2022-02-28 16:29:07 -0300

    Listando e excluindo produtos

--- app/src/main/java/com/udemy/ifoodcl/activity/EmpresaActivity.java
+++ app/src/main/java/com/udemy/ifoodcl/activity/EmpresaActivity.java
@@ -3,31 +3,118 @@ package com.udemy.ifoodcl.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.udemy.ifoodcl.R;
+import com.udemy.ifoodcl.adapter.AdapterProduto;
 import com.udemy.ifoodcl.helper.ConfiguracaoFirebase;
+import com.udemy.ifoodcl.helper.RecyclerItemClickListener;
+import com.udemy.ifoodcl.helper.UsuarioFirebase;
+import com.udemy.ifoodcl.model.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmpresaActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
+    private RecyclerView recyclerProdutos;
+    private AdapterProduto adapterProduto;
+    private List<Produto> produtos = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private String idUsuarioLogado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
 
+        inicializarComponentes();
         autenticacao = ConfiguracaoFirebase.getReferenciaAutenticacao();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Ifood - Empresa");
         setSupportActionBar(toolbar);
+
+        recyclerProdutos.setLayoutManager(new LinearLayoutManager(this));
+        recyclerProdutos.setHasFixedSize(true);
+        adapterProduto = new AdapterProduto(produtos, this);
+        recyclerProdutos.setAdapter(adapterProduto);
+
+        recuperarProdutos();
+
+        recyclerProdutos.addOnItemTouchListener(
+                new RecyclerItemClickListener(this, recyclerProdutos,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Produto produtoSelecionado = produtos.get(position);
+                                produtoSelecionado.remover();
+                                Toast.makeText(EmpresaActivity.this, "Produto excluido com sucesso", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        })
+        );
+
+    }
+
+    private void recuperarProdutos(){
+
+        DatabaseReference produtosRef = firebaseRef
+                .child("produtos")
+                .child(idUsuarioLogado);
+
+        produtosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                produtos.clear();
+
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    produtos.add(ds.getValue(Produto.class));
+                    Log.d("lista", "onDataChange: " + produtos.size());
+                }
+
+                adapterProduto.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void inicializarComponentes(){
+
+        recyclerProdutos = findViewById(R.id.recyclerProdutos);
 
     }
 
--- app/src/main/java/com/udemy/ifoodcl/model/Produto.java
+++ app/src/main/java/com/udemy/ifoodcl/model/Produto.java
@@ -6,11 +6,18 @@ import com.udemy.ifoodcl.helper.ConfiguracaoFirebase;
 public class Produto {
 
     private String idUsuario;
+    private String idProduto;
     private String nome;
     private String descricao;
     private Double preco;
 
     public Produto() {
+
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebase();
+        DatabaseReference produtoRef = firebaseRef
+                .child("produtos");
+        setIdProduto(produtoRef.push().getKey());
+
     }
 
     public void salvar(){
@@ -18,8 +25,27 @@ public class Produto {
         DatabaseReference produtoRef = firebaseRef
                 .child("produtos")
                 .child(getIdUsuario())
-                .push();
+                .child(getIdProduto());
         produtoRef.setValue(this);
+    }
+
+    public void remover(){
+
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebase();
+        DatabaseReference produtoRef = firebaseRef
+                .child("produtos")
+                .child(getIdUsuario())
+                .child(getIdProduto());
+        produtoRef.removeValue();
+
+    }
+
+    public String getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(String idProduto) {
+        this.idProduto = idProduto;
     }
 
     public String getIdUsuario() {
